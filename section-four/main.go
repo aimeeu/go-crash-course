57a5b3ea3bfd5172a7a40b60f52ffe088dbf64f1
Aimee Ukasick <aimee.ukasick@hashicorp.com>
2025-08-25 12:17:00 -0500

    reorganize directories

---
+++ section-four/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* structs
+
+type Car struct {
+	NumTires    int
+	Luxury      bool
+	BucketSeats bool
+	Make        string
+	Model       string
+	Year        int
+}
+
+// * means that Car is a pointer
+func (c *Car) String() string {
+	output := fmt.Sprintf(("Year: %d \n" +
+	                      "Model: %s \n" +
+												"Make: %s \n" +
+												"Luxury: %t \n" +
+												"Bucket Seats: %t \n" +
+												"Number of Tires: %d"),
+												c.Year, c.Model, c.Make, c.Luxury,
+												c.BucketSeats, c.NumTires)
+	return output
+}
+
+func main() {
+    myCar := &Car {
+		NumTires: 4,
+		BucketSeats: false,
+		Luxury: false,
+		Make: "AMC",
+		Model: "Gremlin",
+		Year: 1981,
+	}
+
+	fmt.Println(myCar)
+
+}
+*/
+
+
+// reference types: pointer, slice, map, function, channel
+
+
+
+// interface type
+
+func main() {
+
+}
